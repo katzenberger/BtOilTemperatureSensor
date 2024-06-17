4cd70a5abb7345687d6e9dac38bf61242a13cf80
Katzenberger Péter <peter.katzenberger@idata.hu>
2024-06-17 16:41:45 +0200

    Added analog resolution parameter

--- BtOilTemperatureSensor.ino
+++ BtOilTemperatureSensor.ino
@@ -9,10 +9,11 @@
 #include <BluetoothSerial.h>
 
 #define SENSOR_PIN             A0
-#define REFERENCE_RESISTANCE   2200
-#define NOMINAL_RESISTANCE     2200
+#define REFERENCE_RESISTANCE   47000
+#define NOMINAL_RESISTANCE     47000
 #define NOMINAL_TEMPERATURE    25
 #define B_VALUE                3950
+#define ANALOG_RESOLUTION      4095
 
 //#define USE_PIN
 const char *pin = "1234";
@@ -30,8 +31,22 @@ String device_name = "BT-OilTemperature";
 #define READINGS_NUMBER 10
 #define DELAY_TIME 10
 
-Thermistor* thermistor = NULL;
 BluetoothSerial SerialBT;
+
+NTC_Thermistor originThermistor(
+  SENSOR_PIN,
+  REFERENCE_RESISTANCE,
+  NOMINAL_RESISTANCE,
+  NOMINAL_TEMPERATURE,
+  B_VALUE,
+  ANALOG_RESOLUTION
+);
+
+AverageThermistor thermistor(
+  &originThermistor,
+  READINGS_NUMBER,
+  DELAY_TIME
+);
 
 void setup() {
   Serial.begin(115200);
@@ -43,30 +58,18 @@ void setup() {
     Serial.println("Using PIN");
   #endif
 
-  Thermistor* originThermistor = new NTC_Thermistor(
-    SENSOR_PIN,
-    REFERENCE_RESISTANCE,
-    NOMINAL_RESISTANCE,
-    NOMINAL_TEMPERATURE,
-    B_VALUE
-  );
-  thermistor = new AverageThermistor(
-    originThermistor,
-    READINGS_NUMBER,
-    DELAY_TIME
-  );
 }
 
 void loop() {
-  const double celsius = thermistor->readCelsius();
+  const double celsius = thermistor.readCelsius();
 
   // Output to Serial
-  Serial.print("Oil temperature: ");
+  Serial.print("Oil: ");
   Serial.print(celsius);
   Serial.println(" C");
 
   // Output to BT
-  SerialBT.print("Oil temperature: ");
+  SerialBT.print("Oil: ");
   SerialBT.print(celsius);
   SerialBT.print(" C");
   SerialBT.println();
